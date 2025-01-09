2915b9a638d3bc077c9ee1c408b6c03793cd8918
Verónica González Pons <91873213+Veronicagnzpns@users.noreply.github.com>
2025-01-09 08:23:54 +0100

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, provideAppInitializer } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -7,20 +7,30 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyACYKe4h2-pAh4rW2ENDmGLB3EYsvT8WLg",
+  authDomain: "nueva-e5fd9.firebaseapp.com",
+  projectId: "nueva-e5fd9",
+  storageBucket: "nueva-e5fd9.firebasestorage.app",
+  messagingSenderId: "721493712641",
+  appId: "1:721493712641:web:4c4c65c60a28040f230500",
+  measurementId: "G-C3N8HVJK7Q"
+};
+
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    { provide: , useValue:  {
-      apiKey: "AIzaSyACYKe4h2-pAh4rW2ENDmGLB3EYsvT8WLg",
-      authDomain: "nueva-e5fd9.firebaseapp.com",
-      projectId: "nueva-e5fd9",
-      storageBucket: "nueva-e5fd9.firebasestorage.app",
-      messagingSenderId: "721493712641",
-      appId: "1:721493712641:web:4c4c65c60a28040f230500",
-      measurementId: "G-C3N8HVJK7Q"
-    }},
-    
+  imports: [BrowserModule, 
+            IonicModule.forRoot(),  
+            AppRoutingModule,
+            //provideFirebaseApp(() => initializeApp(firebaseConfig)),
+      ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: 'FirebaseConfig', useValue: firebaseConfig }, 
   ],
   bootstrap: [AppComponent],
 
@@ -28,3 +38,4 @@ import { AppRoutingModule } from './app-routing.module';
 export class AppModule {
 
 }
+
